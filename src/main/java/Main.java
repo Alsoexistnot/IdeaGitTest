0614be20061df6a2201fca55129faf985151a2c5
Alsoexist <ssologub@mail.ru>
2021-10-20 13:49:07 +0300

    Обязательный коммит

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("IDEA and Github.");
         System.out.println("Буду пушить");
         System.out.println("Сначала делаю коммит, потом пушу. Проверка.");
+        System.out.println("Новый бренч");
     }
 }
